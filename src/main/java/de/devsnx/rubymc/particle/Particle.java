d8b452040b303f03899768b6a604fa4e724fc92c
DevSnx <hnl.marvin@gmail.com>
2023-02-22 18:17:06 +0100

    Update

--- src/main/java/de/devsnx/rubymc/particle/Particle.java
+++ src/main/java/de/devsnx/rubymc/particle/Particle.java
@@ -3,13 +3,12 @@ package de.devsnx.rubymc.particle;
 import com.github.fierioziy.particlenativeapi.api.ParticleNativeAPI;
 import com.github.fierioziy.particlenativeapi.api.utils.ParticleException;
 import com.github.fierioziy.particlenativeapi.core.ParticleNativeCore;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Particle extends JavaPlugin {
 
     public static Particle instance;
-
-    public static ParticleNativeAPI ParticleAPI;
 
     @Override
     public void onEnable() {
@@ -23,10 +22,11 @@ public final class Particle extends JavaPlugin {
 
 
     private void loadAPI(){
-        try {
-            ParticleAPI = ParticleNativeCore.loadAPI(getInstance());
-        } catch (ParticleException e) {// optional runtime exception catch
-            // handle error
+        Plugin plugin = this.getServer().getPluginManager().getPlugin("ParticleNativeAPI");
+        if (plugin != null) {
+            //if (!ParticleNativePlugin.isValid()) {
+                // handle error
+            //}
         }
     }
 
@@ -38,9 +38,5 @@ public final class Particle extends JavaPlugin {
 
     public static Particle getInstance() {
         return instance;
-    }
-
-    public static ParticleNativeAPI getApi() {
-        return ParticleAPI;
     }
 }
