1a4c0d00ada04dc79f20c56a95e09685fe017aa4
DevSnx <hnl.marvin@gmail.com>
2023-02-22 16:41:57 +0100

    Update

---
+++ src/main/java/de/devsnx/rubymc/particle/Particle.java
@@ -0,0 +1,21 @@
+package de.devsnx.rubymc.particle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Particle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+    }
+}
